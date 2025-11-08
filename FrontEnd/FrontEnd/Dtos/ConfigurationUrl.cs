61ccdcd7a543a0fa69c44e3569939787e439c80e
Nightdragoon <crackytrini@gmail.com>
2025-11-08 15:02:29 -0600

    Se imjplento frontend de login
    se implemento frontend de login

---
+++ FrontEnd/FrontEnd/Dtos/ConfigurationUrl.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontEnd.Dtos;
+
+public static class ConfigurationUrl
+{
+    public static string GetBaseUrl()
+    {
+        return "http://127.0.0.1:8000";
+    }
+}
--- Web.Api/main.py
+++ Web.Api/main.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine , Select , Insert
 from sqlalchemy.ext.automap import automap_base
 from UsuarioInsertar import UsuarioInsertar
 from UsuarioBuscar import UsuarioBuscar
+from fastapi.middleware.cors import CORSMiddleware
 
 engine = create_engine("mysql+pymysql://upiw3mqa58obep4h:VMqMoO6MuFgXjBt6ddl@b96lcxztraqbollhfyj6-mysql.services.clever-cloud.com:20670/b96lcxztraqbollhfyj6")
 
@@ -11,6 +12,13 @@ Base = automap_base()
 # Reflect the database schema and prepare the base
 Base.prepare(autoload_with=engine)
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/registrarUsuario")
