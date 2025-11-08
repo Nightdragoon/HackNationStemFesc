b6e600f4965eab85e87efda90c0755c8fc33a5aa
Nightdragoon <crackytrini@gmail.com>
2025-11-08 13:03:45 -0600

    Integracion Base de datos
    se integro la base de datos +

--- Web.Api/main.py
+++ Web.Api/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
 
+engine = create_engine("mysql+pymysql://upiw3mqa58obep4h:VMqMoO6MuFgXjBt6ddl@b96lcxztraqbollhfyj6-mysql.services.clever-cloud.com:20670/b96lcxztraqbollhfyj6")
+
+Base = automap_base()
+
+# Reflect the database schema and prepare the base
+Base.prepare(autoload_with=engine)
 app = FastAPI()
 
 
@@ -11,3 +19,4 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
